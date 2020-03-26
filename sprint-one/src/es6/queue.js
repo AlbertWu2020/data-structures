9c2829e775fba8186efb41ef94562d3022406152
Albert <albertwu0001@gmail.com>
2020-03-25 23:05:13 -0700

    Sprint-one completed

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,27 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(storage, sizee) {
+    this.storage = {};
+    this.sizee = 0;
+  }
+  enqueue(val){
+    this.sizee++;
+    this.storage[this.sizee] = val;
+  };
+  dequeue(){
+    var takeOut = this.storage[1];
+    delete this.storage[1];
+    for(var i = 1; i < this.sizee; i++){
+      this.storage[i] = this.storage[1 + i];
+    }
+    if(this.sizee > 0){
+      this.sizee--;
+    }
+    return takeOut;
+  };
+  size(){
+    return this.sizee;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,24 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(storage, siezee) {
+    this.storage = {};
+    this.sizee = 0;
+  }
+  push(val){
+    this.sizee++;
+    this.storage[this.sizee] = val;
+  };
+  pop(){
+    var takeOut = this.storage[this.sizee];
+    delete this.storage[this.sizee];
+    if(this.sizee > 0){
+      this.sizee--;
+    }
+    return takeOut;
+  };
+  size(){
+    return this.sizee;
   }
 
 }

2dd0c69763e19651579b355f86f729aa43f324c4
Albert <albertwu0001@gmail.com>
2020-03-25 21:45:42 -0700

    Functional completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,40 @@
 var Queue = function() {
   var someInstance = {};
-
+  // create a size variable
+  var size = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // add size
+    size ++;
+    // assign value in storage after size add
+    storage[size] = value;
   };
 
   someInstance.dequeue = function() {
+    // check if size > 0
+    if(size > 0){
+      // subtract a size
+      size --;
+    }
+    // create a variable for the one val should be took out
+    var takeOut = storage[1];
+    // delete the val should be took out
+    delete storage[1];
+    // subs the position of the origin val to a new val
+    for(var key in storage){
+      storage[key - 1] = storage[key];
+    }
+    // return take out
+    return takeOut;
   };
 
   someInstance.size = function() {
+    // return size
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var Stack = function() {
   var someInstance = {};
-
+  var size = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size++;
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
+    var takeOut = storage[size];
+    delete storage[size];
+    if(size > 0){
+      size--;
+    }
+    return takeOut;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;

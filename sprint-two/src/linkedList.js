765cb8982baa918a9086a4e2aeb02fa6b6764441
Albert <albertwu0001@gmail.com>
2020-03-26 18:32:27 -0700

    Linkedlist completed

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -8,7 +8,7 @@ var LinkedList = function() {
     var newTail = Node(value);
 
 
- // check if the node is not in the list
+ // check if the head node is not in the list
  if(list.head === null){
    list.head = newTail;
  }
@@ -19,44 +19,35 @@ var LinkedList = function() {
   // set the next value to equal the variable which is value
   list.tail.next = newTail;
  }
-
+ //list.value = newTail.value;
  list.tail = newTail;
 
   };
 
   list.removeHead = function() {
-    var currentHead = list.head;
-    // the new head will be the first head
+    var currentHead = list.head.value;
+    // the next head will be the first head
     list.head = list.head.next;
     // remove the first head from the list
     delete currentHead;
     // return remove value
-    return currentHead.value;
+    return currentHead;
   };
 
   list.contains = function(target) {
     // set variable assign to check the head
-    var checkHead = list.head.value;
-    // console.log(checkHead);
-    // console.log(target);
-    // console.log(list);
-    //while(checkHead){
-       // check the value if it is equal target
-      if(checkHead === target){
+    var checkHead = list.head;
 
+    while(checkHead){
+       // check the value if it is equal target
+      if(checkHead.value === target){
       // return true
         return true;
-      // else
-      }else{
-       // return false
-        return false;
       }
+       checkHead = checkHead.next;
 
-    //}
-
-    // checkHead = list.head.next;
-    // console.log(checkHead);
-
+  }
+      return false;
   };
 
   return list;
@@ -72,5 +63,5 @@ var Node = function(value) {
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? Linear
  */

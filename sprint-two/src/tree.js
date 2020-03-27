a12509b0175eb381fccb9de2feb8b62991041a33
Albert <albertwu0001@gmail.com>
2020-03-26 21:14:51 -0700

    Tree completed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  // merge newTree and treeMethods object into newTree object
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +13,39 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+
+  // create an variable for invoke the tree function
+  var invokeTree = Tree(value);
+  // pushing the new elements to children array
+  this.children.push(invokeTree);
 };
 
 treeMethods.contains = function(target) {
+  // check if the target equal value
+  if(this.value === target){
+    // true
+    return true;
+    // else
+  }else{
+    // iterate over children
+    for(var i=0; i < this.children.length; i++){
+       // check if the value equal target
+       console.log('thissss====', this);
+       console.log('childrennnnn',this.children[i])
+       if(this.children[i].contains(target)){
+
+         // true
+         return true;
+       }
+
+    }
+
+  }
+  // return false for false cases
+  return false;
+
+
+
 };
 
 

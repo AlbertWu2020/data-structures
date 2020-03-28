5043df00ac34e232e355814ebd9904de0d481fcb
Albert <albertwu0001@gmail.com>
2020-03-27 18:36:19 -0700

    Set file  completed

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // add porperties into empty obj
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  // return boolean if we found an item or not
+  return !!this._storage[item];
 };
 
 setPrototype.remove = function(item) {
+  // delete the add val
+  delete this._storage[item];
 };
 
 /*
